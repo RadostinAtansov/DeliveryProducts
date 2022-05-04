9701797ed6661ef014f1467a7ed0c5f3e95cc07b
Radostin Atanasov <hapa_21@abv.bg>
2022-05-04 10:45:34 +0300

    FirstCommit make data and relations

---
+++ InkoOrders.Data/InkoOrders.Data.Model/TransportAndTransactionPayments.cs
@@ -0,0 +1,11 @@
+﻿namespace InkoOrders.Data.Model
+{
+    public class TransportAndTransactionPayments
+    {
+        public int TransportId { get; set; }
+        public Transport Transport { get; set; }
+
+        public int TransactionPaymentsId { get; set; }
+        public TransactionPayment TransactionPayment { get; set; }
+    }
+}
